091903d9cf0a923637803b16912c49a6b4de2051
Jay Patel <jay734883@gmail.com>
2023-02-24 20:50:37 +0530

    added cors

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.json({limit: "50mb"}));
 app.use(express.urlencoded({extended: true, limit: "50mb"}));
 
 app.use(cookieParser());
-app.use(cors());
+app.use(cors())
 
 import { userRoute } from "./routes/User.js";
 
