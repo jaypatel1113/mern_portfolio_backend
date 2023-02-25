09ed60f9c0cc45b9ad1a83c97c611c98158ea7e5
Jay Patel <jay734883@gmail.com>
2023-02-25 10:30:19 +0530

    added origin localhost and frontend url

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ app.use(express.json({limit: "50mb"}));
 app.use(express.urlencoded({extended: true, limit: "50mb"}));
 
 app.use(cookieParser());
-app.use(cors())
+// app.use(cors())
+app.use(cors({credentials: true, origin: ['http://localhost:3000', 'https://patel-jay.netlify.app']}));
 
 import { userRoute } from "./routes/User.js";
 
